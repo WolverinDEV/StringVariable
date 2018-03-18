be7882d3e941340c11500cf0758c6429b8d14996
WolverinDEV <git@mcgalaxy.de>
2018-03-18 12:30:41 +0100

    Some small changes

--- include/StringVariable.h
+++ include/StringVariable.h
@@ -8,22 +8,26 @@
 #include <iostream>
 
 namespace strvar {
-    class StringProperties;
-    class Value;
+	class StringProperties;
 
-    namespace exceptions {
-        class StrvarException : public std::exception {
-            public:
-                StrvarException(std::string message) : message(std::move(message)) {}
-                StrvarException(const StrvarException& ref) : message(ref.message) {}
-                StrvarException(StrvarException&& ref) : message(ref.message) {}
+	class Value;
 
-                const char *what() const noexcept {
-                    return this->message.c_str();
-                }
-            private:
-                const std::string message;
-        };
+	namespace exceptions {
+		class StrvarException : public std::exception {
+			public:
+				StrvarException(std::string message) : message(std::move(message)) {}
+
+				StrvarException(const StrvarException &ref) : message(ref.message) {}
+
+				StrvarException(StrvarException &&ref) : message(ref.message) {}
+
+				const char *what() const noexcept {
+					return this->message.c_str();
+				}
+
+			private:
+				const std::string message;
+		};
 
 #define DefineException(ClassName) \
         class ClassName : public StrvarException { \
@@ -31,126 +35,131 @@ namespace strvar {
                 ClassName(const std::string msg) : StrvarException(msg) {} \
         };
 
-        DefineException(InvalidValueTypeException)
+		DefineException(InvalidValueTypeException)
 
 #undef DefineException
-    }
+	}
 
-    struct Value {
-        public:
-            Value(std::string key) : _key(std::move(key)) {}
+	struct Value {
+		public:
+			explicit Value(std::string key) : _key(std::move(key)) {}
 
-            const std::string key() const { return this->_key; };
-            virtual std::string value(const StringProperties&, std::deque<std::string>) const = 0;
+			const std::string key() const { return this->_key; };
 
-        private:
-            std::string _key;
-    };
+			virtual std::string value(const StringProperties &, std::deque<std::string>) const = 0;
 
-    struct StringValue : public Value {
-        public:
-            StringValue(const std::string& key, std::string value) : Value(key), _value(std::move(value)) { }
+		private:
+			std::string _key;
+	};
 
-            std::string value(const StringProperties&, std::deque<std::string>) const override {
-                return this->_value;
-            }
+	struct StringValue : public Value {
+		public:
+			StringValue(const std::string &key, std::string value) : Value(key), _value(std::move(value)) {}
 
-            void value(const std::string& str) {
-                this->_value = str;
-            }
-        private:
-            std::string _value;
-    };
+			std::string value(const StringProperties &, std::deque<std::string>) const override {
+				return this->_value;
+			}
 
-    struct FunctionValue : public Value {
-            typedef std::function<std::string(std::deque<std::string>)> FValueFNEasy;
-            typedef std::function<std::string(const StringProperties&, std::deque<std::string>)> FValueFN;
-        public:
-            FunctionValue(const std::string& key, const FValueFNEasy &fn)
-                    : Value(std::move(key)), _value([fn](const StringProperties&, std::deque<std::string> val){ return fn(val); }) { }
-            FunctionValue(const std::string& key, FValueFN fn) : Value(std::move(key)), _value(std::move(fn)) { }
+			void value(const std::string &str) {
+				this->_value = str;
+			}
 
-            std::string value(const StringProperties& props, std::deque<std::string> values) const override {
-                return this->_value(props, values);
-            }
-        private:
-            FValueFN _value;
-    };
+		private:
+			std::string _value;
+	};
 
-    struct StringProperties {
-        public:
-            StringProperties() = default;
-            virtual ~StringProperties() = default;
+	struct FunctionValue : public Value {
+			typedef std::function<std::string(std::deque<std::string>)> FValueFNEasy;
+			typedef std::function<std::string(const StringProperties &, std::deque<std::string>)> FValueFN;
+		public:
+			FunctionValue(const std::string &key, const FValueFNEasy &fn)
+					: Value(key), _value([fn](const StringProperties &, std::deque<std::string> val) { return fn(val); }) {}
 
-            std::deque<std::shared_ptr<Value>> avariableValues() { return this->values; }
+			FunctionValue(const std::string &key, FValueFN fn) : Value(key), _value(std::move(fn)) {}
 
-            void registerValue(const std::shared_ptr<Value>& value) {
-                this->values.push_back(value);
-            }
+			std::string value(const StringProperties &props, std::deque<std::string> values) const override {
+				return this->_value(props, values);
+			}
 
-            std::shared_ptr<Value> findValue(const std::string& key) const {
-                for(const auto& value : this->values)
-                    if(value->key() == key)
-                        return value;
-                return nullptr;
-            }
+		private:
+			FValueFN _value;
+	};
 
-            size_t max_deep = 255;
-        private:
-            std::deque<std::shared_ptr<Value>> values;
-    };
+	struct StringProperties {
+		public:
+			StringProperties() = default;
 
-    namespace impl {
-        typedef std::shared_ptr<strvar::Value> ValType;
+			virtual ~StringProperties() = default;
 
-        template<class VariableType, class Enable = void>
-        class VariableTransform {
-            public:
-                static ValType transform(const VariableType&) {
-                    throw exceptions::InvalidValueTypeException(
-                            "Could not convert from " +
-                                    std::string(typeid(VariableType).name()) +
-                                    " to " + std::string(typeid(ValType).name()));
-                }
-        };
+			std::deque<std::shared_ptr<Value>> avariableValues() { return this->values; }
 
-        template <typename VariableType>
-        class VariableTransform<VariableType, typename std::enable_if<std::is_base_of<strvar::Value, VariableType>::value>::type> {
-            public:
-                static ValType transform(const VariableType& value) {
-                    return std::make_shared<VariableType>(value);
-                }
-        };
+			void registerValue(const std::shared_ptr<Value> &value) {
+				this->values.push_back(value);
+			}
 
-        template <typename VariableType>
-        class VariableTransform<VariableType, typename std::enable_if<std::is_convertible<VariableType, ValType>::value>::type> {
-            public:
-                static ValType transform(const VariableType& type) {
-                    return std::dynamic_pointer_cast<strvar::Value>(type);
-                }
-        };
+			std::shared_ptr<Value> findValue(const std::string &key) const {
+				for (const auto &value : this->values)
+					if (value->key() == key)
+						return value;
+				return nullptr;
+			}
 
-        inline void unrollVariadicValues(std::deque<ValType>& list) {}
+			size_t max_deep = 255;
+		private:
+			std::deque<std::shared_ptr<Value>> values;
+	};
 
-        template <typename ValueType, typename... ValueTypes>
-        inline void unrollVariadicValues(std::deque<ValType>& list, const ValueType& value, ValueTypes... values) {
-            list.push_back(VariableTransform<ValueType>::transform(value));
-            unrollVariadicValues(list, values...);
-        }
+	namespace impl {
+		typedef std::shared_ptr<strvar::Value> ValType;
 
-    }
+		//Default transform
+		template<class VariableType, class Enable = void>
+		class VariableTransform {
+			public:
+				static ValType transform(const VariableType &) {
+					throw exceptions::InvalidValueTypeException("Could not convert from " + std::string(typeid(VariableType).name()) + " to " + std::string(typeid(impl::ValType).name()));
+				}
+		};
 
-    extern std::string transform(std::string in, const StringProperties& properties);
+		//Transform RValue
+		template<typename VariableType>
+		class VariableTransform<VariableType, typename std::enable_if<std::is_base_of<strvar::Value, VariableType>::value>::type> {
+			public:
+				static ValType transform(const VariableType &value) {
+					return std::make_shared<VariableType>(value);
+				}
+		};
 
-    template <typename... Values>
-    inline std::string transform(std::string in, Values... values) {
-        std::deque<std::shared_ptr<Value>> mappedValues;
-        impl::unrollVariadicValues(mappedValues, values...);
+		//Transform std::shared_ptr<...>
+		template<typename VariableType>
+		class VariableTransform<VariableType, typename std::enable_if<std::is_convertible<VariableType, ValType>::value>::type> {
+			public:
+				static ValType transform(const VariableType &type) {
+					return std::dynamic_pointer_cast<strvar::Value>(type);
+				}
+		};
 
-        StringProperties properties{};
-        for(const auto& value : mappedValues)
-            properties.registerValue(value);
+		inline void unrollVariadicValues(std::deque<ValType> &list) {}
 
-        return transform(std::move(in), properties);
-    }
+		template<typename ValueType, typename... ValueTypes>
+		inline void unrollVariadicValues(std::deque<ValType> &list, const ValueType &value, ValueTypes... values) {
+			list.push_back(VariableTransform<ValueType>::transform(value));
+			unrollVariadicValues(list, values...);
+		}
+
+	}
+
+	extern std::string transform(std::string in, const StringProperties &properties);
+
+	template<typename... Values>
+	inline std::string transform(std::string in, Values... values) {
+		std::deque<std::shared_ptr<Value>> mappedValues;
+		impl::unrollVariadicValues(mappedValues, values...);
+
+		StringProperties properties{};
+		for (const auto &value : mappedValues)
+			properties.registerValue(value);
+
+		return transform(std::move(in), properties);
+	}
 }
